ebd8d40d30fd41ed836f84a60c52eeb8386ef606
TheExplosion3 <gowl2515@gmail.com>
2025-03-26 10:38:02 -0400

    initial

---
+++ src/main.py
@@ -0,0 +1,7 @@
+import pandas as pd
+
+CJFM_2020 = pd.read_csv('MTA_B2020_CJFM')
+CJFM_2025 = pd.read_csv('MTA_B2025_CJFM')
+OTPM_2020 = pd.read_csv('MTA_B2020_OTPM')
+OTPM_2025 = pd.read_csv('MTA_B2025_OTPM')
+
