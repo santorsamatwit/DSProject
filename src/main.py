8d4a5843d18b20bbce57e7b0c639eba5eaa3a434
TheExplosion3 <gowl2515@gmail.com>
2025-03-26 17:54:07 -0400

    added comments, fixed spacing

--- src/main.py
+++ src/main.py
@@ -1,50 +1,24 @@
 import pandas as pd
 
+# dataset importing
 CJFM_2020 = pd.read_csv('../datasets/MTA_B2020_CJFM.csv')
 CJFM_2025 = pd.read_csv('../datasets/MTA_B2025_CJFM.csv')
 OTPM_2020 = pd.read_csv('../datasets/MTA_B2020_OTPM.csv')
 OTPM_2025 = pd.read_csv('../datasets/MTA_B2025_OTPM.csv')
 
+
+# data cleaning
 CJFM_2020 = CJFM_2020.drop('division', axis=1)
 CJFM_2025 = CJFM_2025.drop('division', axis=1)
 OTPM_2020 = OTPM_2020.drop('division', axis=1)
 OTPM_2025 = OTPM_2025.drop('division', axis=1)
 
-deinterlined = CJFM_2020[CJFM_2020['line'].isin([
-                                                    '1',
-                                                    '6',
-                                                    '7',
-                                                    'G',
-                                                    'L',
-                                                    'S 42nd',
-                                                    'S Fkln',
-                                                    'S Rock'])]
-interlined = CJFM_2020[~CJFM_2020['line'].isin([
-                                                    '1',
-                                                    '6,'
-                                                    '7',
-                                                    'G',
-                                                    'L',
-                                                    'S 42nd',
-                                                    'S Fkln',
-                                                    'S Rock'])]
-current_deinterlined = CJFM_2025[CJFM_2025['line'].isin([
-                                                    '1',
-                                                    '6',
-                                                    '7',
-                                                    'G',
-                                                    'L',
-                                                    'S 42nd',
-                                                    'S Fkln',
-                                                    'S Rock'])]
-current_interlined = CJFM_2025[~CJFM_2025['line'].isin([
-                                                    '1',
-                                                    '6,'
-                                                    '7',
-                                                    'G',
-                                                    'L',
-                                                    'S 42nd',
-                                                    'S Fkln',
-                                                    'S Rock'])]
+# data sorting
+deinterlined = CJFM_2020[CJFM_2020['line'].isin(['1', '6', '7', 'G', 'L', 'S 42nd', 'S Fkln', 'S Rock'])]
+interlined = CJFM_2020[~CJFM_2020['line'].isin(['1', '6', '7', 'G', 'L', 'S 42nd', 'S Fkln','S Rock'])]
+current_deinterlined = CJFM_2025[CJFM_2025['line'].isin(['1', '6', '7', 'G', 'L', 'S 42nd', 'S Fkln', 'S Rock'])]
+current_interlined = CJFM_2025[~CJFM_2025['line'].isin(['1', '6', '7', 'G', 'L', 'S 42nd', 'S Fkln', 'S Rock'])]
+otpm = OTPM_2020[OTPM_2020['line'].isin(['1', '6', '7', 'G', 'L', 'S 42nd', 'S Fkln', 'S Rock'])]
+current_otpm = OTPM_2020[OTPM_2020['line'].isin(['1', '6', '7', 'G', 'L', 'S 42nd', 'S Fkln','S Rock'])]
 
 print(deinterlined)
