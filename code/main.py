e7be96180f3ec3747a7f38f57ccef40a6085bd1b
TheExplosion3 <gowl2515@gmail.com>
2025-04-07 18:39:07 -0400

    reformatting for the new dataset

--- code/main.py
+++ code/main.py
@@ -7,12 +7,35 @@ def arr_averager(a):
     for i in range(0,len(a)):
         a[i] = a[i].mean()
 
-DEINTERLINED_PATHS = {
-    '7S': '7-S-701-726'
-    '7N': '7-N-726-701'
-}
-INTERLINED_PATHS = {
+DEINTERLINED_TERMINI = {
 
+    '1': ['VAN CORTLANDT PARK-242 ST', 'SOUTH FERRY'],
+    '6': ['PELHAM BAY PARK', 'BKLYN BRIDGE CITY HALL'],
+    '7': ['34TH ST HUDSON YARDS', 'FLUSHING-MAIN ST'],
+    'G': ['COURT SQ', 'CHURCH AV'],
+    'L': ['14 ST', '8 AV'],
+    'S42': ['TIMES SQ 42 ST', 'GRAND CENTRAL 42 ST'],
+    'SF': ['FRANKLIN AV', 'PROSPECT PARK'],
+    'SR': ['BROAD CHANNEL', 'ROCKAWAY PARK-BEACH 116 ST']
+}
+INTERLINED_TERMINI = {
+    '2': ['WAKEFIELD-241 ST', 'FLATBUSH AV-BROOKLYN COLLEGE'],
+    '3': ['HARLEM 148 ST', 'NEW LOTS AV'],
+    '4': ['WOODLAWN', 'CROWN HEIGHTS-UTICA AV'],
+    '5': ['EASTCHESTER-DYRE AV', 'FLATBUSH AV-BROOKLYN COLLEGE'],
+    'A': ['INWOOD 207 ST', 'OZONE PARK-LEFFERTS BLVD', 'FAR ROCKAWAY MOTT AV', 'ROCKAWAY PARK-BEACH 116 ST'],
+    'C': ['168 ST', 'EUCLID AV'],
+    'E': ['JAMAICA CENTER-PARSONS/ARCHER', 'WORLD TRADE CENTER'],
+    'B': ['BEDFORD PK BLVD', 'BRIGHTON BEACH'],
+    'D': ['NORWOOD 205 ST', 'STILLWELL AV'],
+    'F': [],
+    'M': [],
+    'N': [],
+    'R': [],
+    'Q': [],
+    'W': [],
+    'J': [],
+    'Z': []
 }
 
 # dataset importing
@@ -21,7 +44,7 @@ DATASET = pd.read_csv('../datasets/MTA_EtoE_Times.csv')
 print(DATASET)
 
 # dataset cleaning
-DATASET = DATASET.drop(['Time Period','Schedule Day Type','Average Speed', 'Average Scheduled Runtime', 'Scheduled Trains', 'Actual Trains', 'Distance', 'Direction', 'Number of Stops', 'Origin Station Name', 'Origin Station ID', 'Destination Station Name', 'Destination Station ID',], axis=1)
+DATASET = DATASET.drop(['Time Period','Schedule Day Type','Average Speed', 'Average Scheduled Runtime', 'Scheduled Trains', 'Actual Trains', 'Distance', 'Direction', 'Number of Stops', 'Stop Path ID' 'Origin Station ID', 'Destination Station ID',], axis=1)
 
 
 
